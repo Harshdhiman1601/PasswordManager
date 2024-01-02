fe183cc9e223a01e21fb814604ac1008d57afe24
Harshdhiman1601 <harshdhiman1601@gmail.com>
2024-01-02 20:11:04 +0530

    program is successful

--- app/src/main/java/com/example/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -9,25 +9,41 @@ import android.util.Log;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    AppDatabase db = Room.databaseBuilder(getApplicationContext(),
-            AppDatabase.class, "myDatabase").build();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppDatabase db = Room.databaseBuilder(this,
+                AppDatabase.class, "myDatabase").allowMainThreadQueries().build();
         PasswordDao passwordDao = db.passwordDao();
-        for(int i = 0; i < 3; i++){
-            Password password = new Password();
-            password.name = "name" + Integer.toString(i);
-            password.password = "pass" + Integer.toString(i);
-
-            passwordDao.insert(password);
-        }
+//        for(int i = 0; i < 3; i++){
+//            Password password = new Password();
+//            password.name = "name" + Integer.toString(i);
+//            password.password = "pass" + Integer.toString(i);
+//
+//            passwordDao.insert(password);
+//        }
         List<Password> passwords = passwordDao.getAll();
         for(int i = 0; i < passwords.size(); i++){
             Log.i("Name: ", (passwords.get(i)).name);
             Log.i("Password: ", passwords.get(i).password);
+
+        }
+//        Password passwordToBeDeleted = passwords.get(0);
+//        passwordDao.DEL(passwordToBeDeleted);
+
+        Password passwordToBeUpdated = new Password();
+        passwordToBeUpdated = passwords.get(0);
+        passwordToBeUpdated.name = "NewName";
+        passwordToBeUpdated.password = "NewPass";
+
+        passwordDao.Update(passwordToBeUpdated);
+        List<Password> newPasswords = passwordDao.getAll();
+        for(int i = 0; i < newPasswords.size(); i++){
+            Log.i("Name: ", (newPasswords.get(i)).name);
+            Log.i("Password: ", newPasswords.get(i).password);
+
         }
     }
 }
--- app/src/main/java/com/example/passwordmanager/PasswordDao.java
+++ app/src/main/java/com/example/passwordmanager/PasswordDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -16,8 +17,8 @@ public interface PasswordDao {
     void insert(Password password);
 
     @Delete
-    void DEL();
-    }
+    void DEL(Password password);
 
-
+    @Update
+    void Update(Password password);
 }
